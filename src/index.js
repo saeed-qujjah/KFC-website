fd71e55d11cf122279cc3fa1ba0aaa082bbbfee5
SaeedKoja <saeedkojastudy@gmail.com>
2024-06-01 22:11:41 +0300

    change username of github

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
